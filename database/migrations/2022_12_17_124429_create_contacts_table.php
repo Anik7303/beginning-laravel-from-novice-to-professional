af057c01d8202a3fc1e7714fc53044704fa5b901
Anik Mohammad <anik7303@gmail.com>
2022-12-18 03:44:36 +0600

    creating Contact Model

---
+++ database/migrations/2022_12_17_124429_create_contacts_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contacts', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('phone')->nullable();
+            $table->string('email');
+            $table->string('address');
+
+            // for laravel 6 or below
+            // $table->unsignedBigInteger('company_id');
+            // $table->bigInteger('company_id', unsigned: true);
+            $table->foreign('company_id')->references('id')->on('companies')->onDelete('cascade');
+            // for laravel 7 and above
+            $table->foreignId('company_id')->constrained()->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contacts');
+    }
+};
