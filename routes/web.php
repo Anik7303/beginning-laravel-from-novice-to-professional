8ae382586c6957044c8f4a1b20b4f2e8759dbdbe
Anik Mohammad <anik7303@gmail.com>
2022-12-25 17:33:29 +0600

    protect routes using auth middleware

--- routes/web.php
+++ routes/web.php
@@ -19,22 +19,26 @@ Route::get('/', function () {
     return view('welcome');
 })->name('index');
 
-Route::get('/contacts', [ContactController::class, 'index'])->name('contacts.index');
+// Route::get('/contacts', [ContactController::class, 'index'])->name('contacts.index')->middleware('auth');
 
-Route::post('/contacts', [ContactController::class, 'store'])->name('contacts.store');
+Route::middleware('auth')->group(function () {
+    Route::get('/contacts', [ContactController::class, 'index'])->name('contacts.index');
 
-Route::get('/contacts/create', [ContactController::class, 'create'])->name('contacts.create');
+    Route::post('/contacts', [ContactController::class, 'store'])->name('contacts.store');
 
-Route::get('/contacts/{id}', [ContactController::class, 'show'])->name('contacts.show');
+    Route::get('/contacts/create', [ContactController::class, 'create'])->name('contacts.create');
 
-Route::put('/contacts/{id}', [ContactController::class, 'update'])->name('contacts.update');
+    Route::get('/contacts/{id}', [ContactController::class, 'show'])->name('contacts.show');
 
-Route::delete('/contacts/{id}', [ContactController::class, 'destroy'])->name('contacts.destroy');
+    Route::put('/contacts/{id}', [ContactController::class, 'update'])->name('contacts.update');
 
-Route::get('/contacts/{id}/edit', [ContactController::class, 'edit'])->name('contacts.edit');
+    Route::delete('/contacts/{id}', [ContactController::class, 'destroy'])->name('contacts.destroy');
+
+    Route::get('/contacts/{id}/edit', [ContactController::class, 'edit'])->name('contacts.edit');
+});
 
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::get('/dashboard', [App\Http\Controllers\HomeController::class, 'index'])->name('dashboard');
+Route::get('/dashboard', [App\Http\Controllers\HomeController::class, 'index'])->name('dashboard')->middleware('auth');
