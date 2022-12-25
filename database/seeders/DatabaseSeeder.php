2648f1d13a1fd6bbe6e53bfb079a88bf2c7dac27
Anik Mohammad <anik7303@gmail.com>
2022-12-26 02:03:33 +0600

    using another way to seed data to database

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,7 +17,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        User::factory()->count(5)->create();
+        // User::factory()->count(5)->create();
         // using model factory
         // Company::factory()->count(10)->create();
         // Contact::factory()->count(50)->create();
@@ -28,6 +28,15 @@ class DatabaseSeeder extends Seeder
         // )->count(rand(5, 10))->create();
 
         // using magic method `has[relationship]`
-        Company::factory()->hasContacts(rand(10, 15))->count(rand(5, 10))->create();
+        // Company::factory()->hasContacts(rand(10, 15))->count(rand(5, 10))->create();
+        User::factory()->count(5)->create()->each(function ($user) {
+            Company::factory()->has(
+                Contact::factory()->count(rand(5, 10))->state(
+                    function ($attributes, Company $company) {
+                        return ['user_id' => $company->user_id];
+                    }
+                )
+            )->count(rand(5, 10))->create(['user_id' => $user->id]);
+        });
     }
 }
