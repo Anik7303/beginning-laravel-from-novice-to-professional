dd7db6c341f2bd62c37ab40c245fd6130aa17b96
Anik Mohammad <anik7303@gmail.com>
2023-01-01 21:42:10 +0600

    added Image model with migration and factory

---
+++ database/factories/ImageFactory.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Image>
+ */
+class ImageFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'title' => $title = $this->faker->sentence(),
+            // 'slug' => Str::slug($title),
+            'slug' => str()->slug($title),
+            'file' => $this->faker->imageUrl($width = 1920, $height = 1080),
+            'dimension' => $width . 'x' . $height,
+            'view_count' => $this->faker->randomNumber(5),
+            'download_count' => $this->faker->randomNumber(5),
+            'is_published' => true,
+            'user_id' => User::factory(),
+        ];
+    }
+}
